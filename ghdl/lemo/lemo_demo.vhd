8d540b0d7069a2b5654e2bad843e555acd3676bb
Hong Cai <hongc@campus-031-097.ucdavis.edu>
2025-05-08 16:30:56 -0700

    simple lemo demo

---
+++ ghdl/lemo/lemo_demo.vhd
@@ -0,0 +1,313 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+use work.common.all;
+
+entity lemo_demo is
+  generic (
+    C_SCOPE       : integer  := 16#F#;  -- GLOBAL
+    C_ROLE        : integer  := 16#1#;  -- DEBUGGING
+    C_REG_SCR     : integer  := 16#0#;
+    C_REG_CONF    : integer  := 16#4#;
+    C_REG_STAT    : integer  := 16#8#;
+    C_REG_COUNT   : integer  := 16#C#;
+    C_REG_BRATE   : integer  := 16#10#;
+    C_REG_BHOLD   : integer  := 16#14#;
+    C_REG_LCOUNT   : integer  := 16#18#;
+    C_REG_LRATE   : integer  := 16#1C#;
+    t_integer     : integer  := 1000000
+    --C_VAL_ROA     : unsigned(31 downto 0)  := x"11111111";
+    -- C_VAL_ROB     : unsigned(31 downto 0)  := x"22222222"
+    );      
+  port (
+    ACLK	              : in std_logic;
+    ARESETN	            : in std_logic;    
+
+    S_REGBUS_RB_RUPDATE : in  std_logic;
+    S_REGBUS_RB_RADDR	  : in  std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+    S_REGBUS_RB_RDATA	  : out std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);      
+    S_REGBUS_RB_RACK    : out  std_logic;
+    
+    S_REGBUS_RB_WUPDATE : in  std_logic;
+    S_REGBUS_RB_WADDR	  : in  std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+    S_REGBUS_RB_WDATA	  : in  std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+    S_REGBUS_RB_WACK    : out  std_logic;
+    
+    LED_Drive           : out std_logic;
+    LEMO                : in std_logic;
+
+    DEBUG               : out  std_logic_vector(C_RB_DATA_WIDTH-1 downto 0)
+    );
+begin
+  assert C_RB_ADDR_WIDTH >= 8;
+end;
+
+architecture behavioral of lemo_demo is
+  signal clk      : std_logic;
+  signal rst      : std_logic;
+
+  signal rupdate  : std_logic;
+  signal raddr    : std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+  signal rdata    : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+  signal rack     : std_logic := '0';
+  
+  signal wupdate  : std_logic;
+  signal waddr    : std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+  signal wdata    : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+  signal wack     : std_logic := '0';
+
+  signal led_enable     : std_logic :='0';
+  signal led_switch     : std_logic :='0';
+  signal lemo_enable    : std_logic :='0';
+  --signal lemo_switch  : std_logic :='0';
+
+
+  -- registers
+  signal scr             : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0) := (others => '0');
+
+  signal stat            : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0) := (others => '0');
+
+  signal counter_led     : integer :=0;
+  signal config          : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0) := (others => '0');
+  signal brate           : integer :=0;
+  signal bhold           : integer :=0;
+  signal counter_lemo    : integer :=0;
+  signal meta            : std_logic;
+  signal stat_old        : std_logic;
+  signal rate_lemo       : integer :=0;
+ 
+begin
+  DEBUG <= scr;
+  clk <= ACLK;
+  rst <= not ARESETN;
+
+  --outputs:
+  S_REGBUS_RB_RDATA	 <= rdata;
+  S_REGBUS_RB_RACK	 <= rack;
+  S_REGBUS_RB_WACK	 <= wack;
+  
+
+  --inputs: (already registered at preceding stage)
+  rupdate  <= S_REGBUS_RB_RUPDATE;
+  raddr    <= S_REGBUS_RB_RADDR;
+  wupdate  <= S_REGBUS_RB_WUPDATE;
+  waddr    <= S_REGBUS_RB_WADDR;
+  wdata    <= S_REGBUS_RB_WDATA;
+
+  
+
+  --mode 
+
+  process(clk,rst) 
+  begin
+    if (rst = '1') then
+      led_enable <= '0';
+      lemo_enable <= '0';
+    else
+      if(rising_edge(clk)) then
+        if (brate > 0 and bhold >0) then
+          led_enable <= config(0);
+        else
+          led_enable <= '0';
+        end if;    
+        lemo_enable <= config(1);
+      end if;
+    end if;   
+  end process;
+
+
+ --Blink LED
+  process(clk,rst)
+  variable count   : integer :=0;
+  begin
+    if (rst = '1') then
+      count := 0;
+      counter_led <=0;
+      led_switch <= '0';
+    else
+      if(rising_edge(clk)) then
+        if (led_enable = '1') then        
+                       
+            if (count mod brate < bhold) then
+              led_switch <= '1';
+            else
+              led_switch <= '0';
+            end if;
+
+            if (count = brate -1 ) then
+              count := 0;
+            else 
+              count := count + 1;
+            end if; 
+
+            if (count = 0) then
+              if (counter_led = 10000000 -1 ) then
+                counter_led <=0;
+              else  
+                counter_led <= counter_led + 1;
+              end if;  
+            end if;
+  
+        end if;
+      end if;  
+    end if;
+  end process;
+
+  --lemo
+  process(clk,rst)  
+  variable count   : integer :=0;
+--  variable count_old   : integer :=0;
+  variable count_p :integer :=0;
+  begin
+    if (rst = '1') then
+      rate_lemo <= 0;
+      counter_lemo <=0;
+      stat_old <= '0';
+    else
+      if(rising_edge(clk)) then
+        if (count >= t_integer ) then
+          count :=0;
+        else
+          count := count + 1;
+        end if;
+        
+        if (lemo_enable = '1') then        
+          meta    <= LEMO;
+          stat(0) <= meta;
+          stat_old   <= stat(0);
+            
+          if (stat_old = '0' and stat(0) = '1') then
+            if (count >= t_integer ) then
+              rate_lemo <= count_p;
+              count_p :=0;
+            else
+              count_p := count_p +1 ;
+
+            end if;
+
+              if (counter_lemo = 10000000 -1 ) then
+                counter_lemo <=0;
+              else
+                counter_lemo <= counter_lemo + 1;
+              end if;
+--              count_old := count;
+            end if;  
+        end if;
+      end if;  
+    end if;
+  end process;
+
+
+
+
+
+
+  -- Handle Read Request:
+  process(clk,rst)
+  variable scope   : integer;
+  variable role    : integer;
+  variable reg     : integer;
+  begin  
+    if (rst = '1') then
+      rdata <= x"00000000";
+      rack <= '0';
+    else
+      if (rising_edge(clk)) then
+        if (rupdate='0') then
+          --rdata is registered until the next update or reset.
+          rack <= '0';
+        else
+          scope := to_integer(unsigned(raddr(15 downto 12)));
+          role  := to_integer(unsigned(raddr(11 downto 8)));
+          reg   := to_integer(unsigned(raddr(7 downto 0)));          
+          if ((scope=C_SCOPE) and (role=C_ROLE)) then
+            if (reg=C_REG_SCR) then
+              rdata <= scr;
+              rack  <= '1';
+            elsif (reg=C_REG_CONF) then
+              rdata <= config;
+              rack  <= '1'; 
+            elsif (reg=C_REG_STAT) then
+              rdata <= stat;
+              rack  <= '1';  
+            elsif (reg=C_REG_COUNT) then
+                rdata <= std_logic_vector(to_signed(counter_led + 1 ,32));
+              rack  <= '1'; 
+            elsif (reg=C_REG_LCOUNT) then
+                rdata <= std_logic_vector(to_signed(counter_lemo + 1 ,32));
+              rack  <= '1';      
+            elsif (reg=C_REG_BRATE) then
+              rdata <= std_logic_vector(to_signed(brate,32));
+      
+              rack  <= '1';
+            elsif (reg=C_REG_BHOLD) then
+              rdata <= std_logic_vector(to_signed(bhold,32));
+              rack  <= '1'; 
+            elsif (reg=C_REG_LRATE) then
+              rdata <= std_logic_vector(to_signed(rate_lemo,32));
+              rack  <= '1';   
+            else
+              -- this is an error, invalid register
+              rdata <= x"EEEEEEEE";
+              rack  <= '0';
+            end if;
+          else
+            -- this is not an error, just a request outside our scope/role
+            rdata <= x"00000000";
+            rack  <= '0';
+          end if;
+        end if;
+      end if;
+    end if; 
+  end process;
+
+  -- Handle Write Request:
+  process(clk,rst)
+  variable scope   : integer;
+  variable role    : integer;
+  variable reg     : integer;
+  begin  
+    if (rst = '1') then
+      scr <= x"00000000";
+--      config <= x"00000000";
+      brate <= 0;
+      bhold <= 0;
+    else
+      if (rising_edge(clk)) then
+        if (wupdate='0') then
+          wack  <= '0';          
+        else
+          scope := to_integer(unsigned(waddr(15 downto 12)));
+          role  := to_integer(unsigned(waddr(11 downto 8)));
+          reg   := to_integer(unsigned(waddr(7 downto 0)));              
+          if ((scope=C_SCOPE) and (role=C_ROLE)) then
+            if (reg=C_REG_SCR) then
+              scr<= wdata;
+              wack  <= '1';
+            elsif (reg=C_REG_CONF) then
+              config<= wdata;
+              wack  <= '1';
+            elsif (reg=C_REG_BRATE) then
+              brate<= to_integer(signed(wdata));
+              wack  <= '1';
+            elsif (reg=C_REG_BHOLD) then
+              bhold<= to_integer(signed(wdata));
+              wack  <= '1';
+              
+            else
+                -- this is an error, invalid register
+              wack  <= '0';
+            end if;  
+          
+          else
+            -- this is not an error, just a request outside our scope/role
+            wack  <= '0';
+          end if ;
+        end if;
+      end if;
+    end if;   
+  end process;
+
+  LED_Drive <=led_enable and led_switch;
+end; 
---
+++ ghdl/lemo/lemo_demo_tb.vhd
@@ -0,0 +1,222 @@
+library ieee;
+use std.textio.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_textio.all;  -- use -fsynopsys or --std=08 
+library work;
+use work.common.all;
+
+--  Defines a testbench (without any ports)
+entity lemo_demo_tb is
+end lemo_demo_tb;
+     
+architecture behaviour of lemo_demo_tb is
+  component lemo_demo is
+    port (
+      ACLK	                 : in std_logic;
+      ARESETN	               : in std_logic;
+
+
+      S_REGBUS_RB_RADDR	     : in  std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+      S_REGBUS_RB_RDATA	     : out std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+      S_REGBUS_RB_RUPDATE    : in  std_logic;
+      S_REGBUS_RB_RACK       : out std_logic;
+      
+      S_REGBUS_RB_WUPDATE    : in  std_logic;
+      S_REGBUS_RB_WADDR	     : in  std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0);
+      S_REGBUS_RB_WDATA	     : in  std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+      S_REGBUS_RB_WACK       : out std_logic;
+      DEBUG                  : out  std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+
+
+      LED_Drive             : out std_logic;
+      LEMO                   : in std_logic
+      );
+  end component;
+
+  
+  signal count    : integer := 0;
+  signal aclk     : std_logic;
+  signal aresetn  : std_logic;
+
+ --drive
+  signal led    : std_logic;
+  signal lemo   : std_logic;
+  -- read signals:
+  signal raddr   : std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0) := (others => '0');
+  signal rupdate : std_logic := '0';
+  signal rdata   : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0);
+  signal rack    : std_logic := '0';
+  -- write signals:
+  signal waddr   : std_logic_vector(C_RB_ADDR_WIDTH-1 downto 0) := (others => '0');
+  signal wupdate : std_logic := '0';
+  signal wdata   : std_logic_vector(C_RB_DATA_WIDTH-1 downto 0) := (others => '0');
+  signal wack    : std_logic := '0';  
+begin
+  uut: lemo_demo port map (
+      ACLK           => aclk,
+      ARESETN        => aresetn,      
+      S_REGBUS_RB_RUPDATE => rupdate,
+      S_REGBUS_RB_RADDR   => raddr,
+      S_REGBUS_RB_RDATA   => rdata,
+      S_REGBUS_RB_RACK    => rack,
+      S_REGBUS_RB_WUPDATE => wupdate,
+      S_REGBUS_RB_WADDR   => waddr,
+      S_REGBUS_RB_WDATA   => wdata,
+      S_REGBUS_RB_WACK    => wack,
+
+      LED_Drive => led,
+      LEMO => lemo
+      
+      );
+  
+  aresetn_process : process
+  begin
+    aresetn <= '0';
+    wait for 12 ns;
+    aresetn <= '1';    
+    wait;
+  end process;
+  
+  aclk_process : process
+
+  begin 
+    count <= count + 1;   
+    aclk <= '1';
+    wait for 5 ns;
+    aclk <= '0';
+    wait for 5 ns;
+  end process;
+
+  lemo_process : process
+  begin   
+    lemo   <= '1';
+    wait for 10 ns;
+    lemo  <= '0';
+    wait for 10 ns;
+    lemo  <= '1';
+    wait for 10 ns;
+    lemo  <= '0';
+    wait for 10 ns;
+    lemo  <= '1';
+    wait for 10 ns;
+    lemo  <= '0';
+    wait for 10 ns;
+    lemo  <= '1';
+    wait for 20 ns;
+    lemo  <= '0';
+    wait for 20 ns;
+    lemo  <= '1';
+  end process;  
+
+  rapid_read_process : process
+  begin
+    raddr   <= x"0000";
+    rupdate <= '0';
+--    wait for 8 ns;
+--    wait for 30 ns;
+--    raddr   <= x"F110";
+--    rupdate <= '1';
+--    wait for 10 ns;
+--    raddr   <= x"F114";
+--    rupdate <= '1';
+--    wait for 10 ns;
+--    raddr   <= x"F108";
+--    rupdate <= '1';
+--    wait for 10 ns;
+--    raddr   <= x"0000";
+--    rupdate <= '0';
+--    wait for 30 ns;
+--    raddr   <= x"F100";
+--    rupdate <= '1';    
+    wait for 10 ns;    
+    raddr   <= x"F104";
+    rupdate <= '1';    
+--    wait for 10 ns;  
+--    wait for 10 ns;
+--    raddr   <= x"F110";
+--    rupdate <= '1';
+--    wait for 10 ns;
+--    raddr   <= x"F114";
+--    rupdate <= '1';
+    wait for 30 ns; 
+    raddr   <= x"F118";
+    rupdate <= '1'; 
+     wait for 10 ns;
+    raddr   <= x"F11C";
+    rupdate <= '1'; 
+    wait;
+  end process;
+
+  rapid_write_process : process
+  begin
+    wait for 1 ns;
+    --wait for 20 ns;
+    waddr   <= x"F100";
+    wdata   <= x"FEEDDADA";
+    wupdate <= '1';
+    wait for 10 ns;
+    waddr   <= x"F104";
+    wdata   <= x"00000003";
+    wupdate <= '1';
+    wait for 10 ns;
+    waddr   <= x"F114";
+    wdata   <= x"00000003";
+    wupdate <= '1';
+    wait for 10 ns;
+    waddr   <= x"F110";
+    wdata   <= x"0000000A";
+    wupdate <= '1';
+    wait for 10 ns;
+    --expert write:
+    waddr   <= x"F1E0";
+    wdata   <= x"FFFFFFFF";
+    wupdate <= '1';    
+    wait for 10 ns;
+    waddr   <= x"0000"; 
+    wdata   <= x"00000000";
+    wupdate <= '0';
+    wait;
+  end process;
+
+  output_process : process
+    variable l : line;
+  begin
+    --wait for 1 ns;
+    if (count < 60) then
+      wait for 10 ns;
+    else
+      wait;
+    end if;
+    write (l, String'("c: "));
+    write (l, count, left, 4);
+    --write (l, String'("aclk: "));
+    --write (l, aclk);
+    write (l, String'(" || ra: 0x"));
+    hwrite (l, raddr);
+    write (l, String'(" ru:"));
+    write (l, rupdate);
+    write (l, String'(" rd: 0x"));
+    hwrite (l, rdata);
+    write (l, String'(" rk:"));
+    write (l, rack);
+    write (l, String'(" || wa: 0x"));
+    hwrite (l, waddr);
+    write (l, String'(" wu:"));
+    write (l, wupdate);
+    write (l, String'(" wd: 0x"));
+    hwrite (l, wdata);
+    write (l, String'(" wk:"));
+    write (l, wack);
+    write (l, String'(" led:"));
+    write (l, led);
+    write (l, String'(" lemo:"));
+    write (l, lemo);
+    if (aresetn = '0') then
+      write (l, String'(" (RESET)"));
+    end if;
+    writeline(output, l);
+  end process;
+  
+end behaviour;
+        
